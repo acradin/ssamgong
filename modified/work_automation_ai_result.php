761a7d3590490a493a8ddd724c651eef03cc3e3c
Kou0923 <ropa0923@gmail.com>
2025-05-26 12:06:30 +0900

    네비게이션 추가

--- modified/work_automation_ai_result.php
+++ modified/work_automation_ai_result.php
@@ -195,6 +195,7 @@ foreach ($chatSessions as $session) {
                     </li>
                     <li class=""><a href="./item_e_book">전자책</a></li>
                     <li class=""><a href="./community_communication">커뮤니티</a></li>
+                    <li class="on"><a href="./work_automation_ai">업무 자동화</a></li>
                 </ul>
             </div>
 
